4a5716d358007ff80daa73dc8fec4224e27b9a6c
integer-bin <integerbin@naver.com>
2024-08-11 19:25:31 +0900

    #6 로그인 페이지만 구현했음
    제곧내
    지원아 홧팅해라

--- template/React-Landing-Page-Template/src/components/login.jsx
+++ template/React-Landing-Page-Template/src/components/login.jsx
@@ -1,165 +1,261 @@
 import { useState } from "react";
 import emailjs from "emailjs-com";
 import React from "react";
+import { useNavigate } from 'react-router-dom'
 
-const initialState = {
-  name: "",
-  email: "",
-  message: "",
-};
+
+// const initialState = {
+//   name: "",
+//   email: "",
+//   message: "",
+// };
+
+
+
+// export const Login = (props) => {
+//   const [{ name, email, message }, setState] = useState(initialState);
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setState((prevState) => ({ ...prevState, [name]: value }));
+//   };
+//   const clearState = () => setState({ ...initialState });
+  
+  
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     console.log(name, email, message);
+    
+//     {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
+    
+//     emailjs
+//       .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+//       .then(
+//         (result) => {
+//           console.log(result.text);
+//           clearState();
+//         },
+//         (error) => {
+//           console.log(error.text);
+//         }
+//       );
+//   };
+
+//   return (
+//     <div>
+//       <div id="login">
+//         <div className="container">
+//           <div className="col-md-8">
+//             <div className="row">
+//               <div className="section-title">
+//                 <h2>Get In Touch</h2>
+//                 <p>
+//                   Please fill out the form below to send us an email and we will
+//                   get back to you as soon as possible.
+//                 </p>
+//               </div>
+//               <form name="sentMessage" validate onSubmit={handleSubmit}>
+//                 <div className="row">
+//                   <div className="col-md-6">
+//                     <div className="form-group">
+//                       <input
+//                         type="text"
+//                         id="name"
+//                         name="name"
+//                         className="form-control"
+//                         placeholder="Name"
+//                         required
+//                         onChange={handleChange}
+//                       />
+//                       <p className="help-block text-danger"></p>
+//                     </div>
+//                   </div>
+//                   <div className="col-md-6">
+//                     <div className="form-group">
+//                       <input
+//                         type="email"
+//                         id="email"
+//                         name="email"
+//                         className="form-control"
+//                         placeholder="Email"
+//                         required
+//                         onChange={handleChange}
+//                       />
+//                       <p className="help-block text-danger"></p>
+//                     </div>
+//                   </div>
+//                 </div>
+//                 <div className="form-group">
+//                   <textarea
+//                     name="message"
+//                     id="message"
+//                     className="form-control"
+//                     rows="4"
+//                     placeholder="Message"
+//                     required
+//                     onChange={handleChange}
+//                   ></textarea>
+//                   <p className="help-block text-danger"></p>
+//                 </div>
+//                 <div id="success"></div>
+//                 <button type="submit" className="btn btn-custom btn-lg">
+//                   Send Message
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//           <div className="col-md-3 col-md-offset-1 login-info">
+//             <div className="login-item">
+//               <h3>login Info</h3>
+//               <p>
+//                 <span>
+//                   <i className="fa fa-map-marker"></i> Address
+//                 </span>
+//                 {props.data ? props.data.address : "loading"}
+//               </p>
+//             </div>
+//             <div className="login-item">
+//               <p>
+//                 <span>
+//                   <i className="fa fa-phone"></i> Phone
+//                 </span>{" "}
+//                 {props.data ? props.data.phone : "loading"}
+//               </p>
+//             </div>
+//             <div className="login-item">
+//               <p>
+//                 <span>
+//                   <i className="fa fa-envelope-o"></i> Email
+//                 </span>{" "}
+//                 {props.data ? props.data.email : "loading"}
+//               </p>
+//             </div>
+//           </div>
+//           <div className="col-md-12">
+//             <div className="row">
+//               <div className="social">
+//                 <ul>
+//                   <li>
+//                     <a href={props.data ? props.data.facebook : "/"}>
+//                       <i className="fa fa-facebook"></i>
+//                     </a>
+//                   </li>
+//                   <li>
+//                     <a href={props.data ? props.data.twitter : "/"}>
+//                       <i className="fa fa-twitter"></i>
+//                     </a>
+//                   </li>
+//                   <li>
+//                     <a href={props.data ? props.data.youtube : "/"}>
+//                       <i className="fa fa-youtube"></i>
+//                     </a>
+//                   </li>
+//                 </ul>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <div id="footer">
+//         <div className="container text-center">
+//           <p>
+//             &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+//             <a href="http://www.templatewire.com" rel="nofollow">
+//               TemplateWire
+//             </a>
+//           </p>
+//         </div>
+//       </div>
+//     </div>
+    
+    
+//   );
+// };
+
+
+// =============================================
+
 export const Login = (props) => {
-  const [{ name, email, message }, setState] = useState(initialState);
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [passwordError, setPasswordError] = useState('')
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({ ...prevState, [name]: value }));
-  };
-  const clearState = () => setState({ ...initialState });
+  const navigate = useNavigate()
+
+  const onButtonClick = () => {
+    // Set initial error values to empty
+    setEmailError('')
+    setPasswordError('')
   
+    // Check if the user has entered both fields correctly
+    if ('' === email) {
+      setEmailError('Please enter your email')
+      return
+    }
   
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, message);
-    
-    {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
-    
-    emailjs
-      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
-      .then(
-        (result) => {
-          console.log(result.text);
-          clearState();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-  };
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      setEmailError('Please enter a valid email')
+      return
+    }
+  
+    if ('' === password) {
+      setPasswordError('Please enter a password')
+      return
+    }
+  
+    if (password.length < 7) {
+      setPasswordError('The password must be 8 characters or longer')
+      return
+    }
+  
+    // Authentication calls will be made here...
+
+
+  }
+
   return (
-    <div>
-      <div id="contact">
-        <div className="container">
-          <div className="col-md-8">
-            <div className="row">
-              <div className="section-title">
-                <h2>Get In Touch</h2>
-                <p>
-                  Please fill out the form below to send us an email and we will
-                  get back to you as soon as possible.
-                </p>
-              </div>
-              <form name="sentMessage" validate onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        className="form-control"
-                        placeholder="Name"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        className="form-control"
-                        placeholder="Email"
-                        required
-                        onChange={handleChange}
-                      />
-                      <p className="help-block text-danger"></p>
-                    </div>
-                  </div>
-                </div>
-                <div className="form-group">
-                  <textarea
-                    name="message"
-                    id="message"
-                    className="form-control"
-                    rows="4"
-                    placeholder="Message"
-                    required
-                    onChange={handleChange}
-                  ></textarea>
-                  <p className="help-block text-danger"></p>
-                </div>
-                <div id="success"></div>
-                <button type="submit" className="btn btn-custom btn-lg">
-                  Send Message
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className="col-md-3 col-md-offset-1 contact-info">
-            <div className="contact-item">
-              <h3>Contact Info</h3>
-              <p>
-                <span>
-                  <i className="fa fa-map-marker"></i> Address
-                </span>
-                {props.data ? props.data.address : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-phone"></i> Phone
-                </span>{" "}
-                {props.data ? props.data.phone : "loading"}
-              </p>
-            </div>
-            <div className="contact-item">
-              <p>
-                <span>
-                  <i className="fa fa-envelope-o"></i> Email
-                </span>{" "}
-                {props.data ? props.data.email : "loading"}
-              </p>
-            </div>
-          </div>
-          <div className="col-md-12">
-            <div className="row">
-              <div className="social">
-                <ul>
-                  <li>
-                    <a href={props.data ? props.data.facebook : "/"}>
-                      <i className="fa fa-facebook"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.twitter : "/"}>
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className={'mainContainer'}>
+      <div className={'titleContainer'}>
+        <div>Login</div>
       </div>
-      <div id="footer">
-        <div className="container text-center">
-          <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
-          </p>
-        </div>
+      <br />
+      <div className={'inputContainer'}>
+        <input
+          value={name}
+          placeholder="Enter your name"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={'inputBox'}
+        />
+      </div>     
+      <br />
+       
+      <div className={'inputContainer'}>
+        <input
+          value={email}
+          placeholder="Enter your email here"
+          onChange={(ev) => setEmail(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{emailError}</label>
+      </div>
+      <br />
+
+      <div className={'inputContainer'}>
+        <input
+          value={password}
+          placeholder="Enter your password here"
+          onChange={(ev) => setPassword(ev.target.value)}
+          className={'inputBox'}
+        />
+        <label className="errorLabel">{passwordError}</label>
+      </div>
+      <br />
+
+      <div className={'inputContainer'}>
+        <input className={'inputButton'} type="button" onClick={onButtonClick} value={'Log in'} />
       </div>
     </div>
-  );
-};
+  )
+}
